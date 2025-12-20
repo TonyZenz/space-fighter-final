0ce9b92111cce923d834430bd94e3a1eb64e9bd1
TonyZenz <zenzer4@gmail.com>
2025-12-20 17:06:51 -0600

    Add files via upload

---
+++ EnemyShip.cpp
@@ -0,0 +1,52 @@
+
+#include "EnemyShip.h"
+
+
+EnemyShip::EnemyShip()
+{
+	SetMaxHitPoints(1);
+	SetCollisionRadius(20);
+}
+
+
+void EnemyShip::Update(const GameTime& gameTime)
+{
+	if (m_delaySeconds > 0)
+	{
+		m_delaySeconds -= gameTime.GetElapsedTime();
+
+		if (m_delaySeconds <= 0)
+		{
+			GameObject::Activate();
+		}
+	}
+
+	if (IsActive())
+	{
+		m_activationSeconds += gameTime.GetElapsedTime();
+		if (m_activationSeconds > 2 && !IsOnScreen()) Deactivate();
+	}
+
+	Ship::Update(gameTime);
+}
+
+
+void EnemyShip::Initialize(const Vector2 position, const double delaySeconds)
+{
+	SetPosition(position);
+	m_delaySeconds = delaySeconds;
+
+	Ship::Initialize();
+}
+
+
+void EnemyShip::Hit(const float damage)
+{
+	Ship::Hit(damage);
+
+	// Increments player score by 100 for every ship destroyed
+	if (!IsActive())
+	{
+		Ship::AddScore(100);
+	}
+}
---
+++ GameplayScreen.cpp
@@ -0,0 +1,56 @@
+
+#include "GameplayScreen.h"
+#include "MainMenuScreen.h"
+#include "Level.h"
+#include "Level01.h"
+#include "GameOverScreen.h"
+
+GameplayScreen::GameplayScreen(const int levelIndex)
+	: m_levelIndex(levelIndex)
+{
+	SetTransitionInTime(1);
+	SetTransitionOutTime(3);
+
+	// Changed to redirect to game over screen instead of main menu
+	SetOnRemove([this](){ AddScreen(new GameOverScreen()); });
+
+	Show();
+}
+
+void GameplayScreen::LoadContent(ResourceManager& resourceManager)
+{
+	m_pResourceManager = &resourceManager;
+	LoadLevel(m_levelIndex);
+}
+
+void GameplayScreen::LoadLevel(const int levelIndex)
+{
+	if (m_pLevel) delete m_pLevel;
+
+	switch (levelIndex)
+	{
+	case 0: m_pLevel = new Level01(); break;
+	}
+
+	m_pLevel->SetGameplayScreen(this);
+	m_pLevel->LoadContent(*m_pResourceManager);
+}
+
+void GameplayScreen::HandleInput(const InputState& input)
+{
+	m_pLevel->HandleInput(input);
+}
+
+void GameplayScreen::Update(const GameTime& gameTime)
+{
+	m_pLevel->Update(gameTime);
+}
+
+void GameplayScreen::Draw(SpriteBatch& spriteBatch)
+{
+	spriteBatch.Begin();
+
+	m_pLevel->Draw(spriteBatch);
+
+	spriteBatch.End();
+}
